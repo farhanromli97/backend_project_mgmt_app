40505676b596850425c96243f004a93737b3c599
Farhan Romli <farhanromli@Farhans-MacBook-Air.local>
2023-11-29 19:15:58 +0800

    fix env variable name for jwt secret

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -4,7 +4,7 @@ const isAuthenticated = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
         console.log(token)
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         console.log(decoded)
         if(!decoded?.id) return res.status(401).json({message: "Unauthorised"})
         req.userId = decoded.id
